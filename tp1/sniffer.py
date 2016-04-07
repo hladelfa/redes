9525fd83636c45ddf790a735a5f7ddc2c40cc061
hladlefa <nahuel.ladelfa@avantrip.com>
2016-04-07 16:02:53 -0300

    se pasa a archivos las salidas de la entropia

--- tp1/sniffer.py
+++ tp1/sniffer.py
@@ -2,11 +2,15 @@
 from scapy.all import *
 
 total_packets = 0.0
+total_ips = 0.0
 packet_types = {}
 packet_names = {}
+ips = {}
 in_timeout = 10
 in_filter_arp = "" 
 file
+arp_file = ""
+type_file = ""
 arp_text= {
     1 : "ARP:who-has",
     2 : "ARP:is-at"
@@ -51,17 +55,17 @@ def writeNodeToFile(pkt,file):
 
     packetInfo += '\n'
     file.write(packetInfo)
+    print(packetInfo)
 
 def monitor_callback(pkt):
 
     
     writeNodeToFile(pkt,file)
 
-    if in_filter_arp != "":
-        arp_entropy_values(pkt)
-    else:         
-        entropy_values(pkt)    
-    print pkt.show()
+    if in_filter_arp != "arp":
+    	entropy_values(pkt) 
+
+    arp_entropy_values(pkt)   
 
 def entropy_values(pkt):
     global total_packets 
@@ -79,43 +83,58 @@ def entropy_values(pkt):
         packet_names[key] = pkt[1].name
 
 def arp_entropy_values(pkt):
-    global total_packets 
-    total_packets += 2 
-    psrc = pkt[ARP].psrc
-    pdst = pkt[ARP].pdst
-    if packet_types.get(psrc) != None:
-        packet_types[psrc] += 1  
-    else:
-        packet_types[psrc] = 1
-        packet_names[psrc] = pkt[1].name
+    global total_ips 
+    
+    if pkt[1].name == 'ARP':	
+    	total_ips += 2 
+    	psrc = pkt[ARP].psrc
+    	pdst = pkt[ARP].pdst
+    	if ips.get(psrc) != None:
+        	ips[psrc] += 1  
+    	else:
+        	ips[psrc] = 1
 
-    if packet_types.get(pdst) != None:
-        packet_types[pdst] += 1
-    else:
-        packet_types[pdst] = 1
-        packet_names[pdst] = pkt[1].name     
-
+    	if ips.get(pdst) != None:
+        	ips[pdst] += 1
+    	else:
+        	ips[pdst] = 1
+        
 def calculate_entropy():
     global total_packets
     global packet_types     
     print total_packets
     entropy = 0.0
+    arp_entropy = 0.0
 
     for type in packet_types:
-        print packet_names[type]+"("+str(type)+") Paquetes: "+str(packet_types[type])+"\n"
+        type_file.write(packet_names[type]+"("+str(type)+") Paquetes: "+str(packet_types[type])+"\n")
         prob = packet_types[type]/total_packets
-        print packet_names[type]+" P("+str(type)+") = "+str(prob)+"\n"
+        type_file.write(packet_names[type]+" P("+str(type)+") = "+str(prob)+"\n")
         info = -math.log(prob,2)
-        print packet_names[type]+" I("+str(type)+") = "+str(info)+"\n"
+        type_file.write(packet_names[type]+" I("+str(type)+") = "+str(info)+"\n")
         entropy = entropy + (prob * info)
 
-    print "H(S) = "+str(entropy)+"\n"	
+    type_file.write("H(S) = "+str(entropy)+"\n")
 
-file = open('workfile', 'w')
+    for ip in ips:
+        arp_file.write(ip+"Paquetes: "+str(ips[ip])+"\n")
+        prob = ips[ip]/total_ips
+        arp_file.write(ip+" P("+ip+") = "+str(prob)+"\n")
+        info = -math.log(prob,2)
+        arp_file.write(ip+" I("+ip+") = "+str(info)+"\n")
+        arp_entropy = arp_entropy + (prob * info)
+
+    arp_file.write("H(S) = "+str(arp_entropy)+"\n")	
+
+fecha = time.strftime("%d-%m-%y-%H:%M:%S")
+file = open('nodes_'+fecha, 'w')
 if __name__ == '__main__':
         sniff(prn=monitor_callback, timeout = in_timeout, filter = in_filter_arp)
 file.close()
 
+arp_file = open('arp_entropy_'+fecha, 'w')
+type_file = open('type_entropy_'+fecha, 'w')
 calculate_entropy()
-
+arp_file.close()
+type_file.close()
 
